7294ea9cd0f35afa8e53f4479fc5a4849c2ff0d9
Sunil Unka <sunil.unka@gmail.com>
2014-07-11 19:04:59 +0000

    Completed get_names module

--- main.rb
+++ main.rb
@@ -1,17 +1,20 @@
-players = {
+@players = {
   :pOne => {
-    "lives" => 3,
-    "correct" => 0,
-    "incorrect" => 0
+    'name' => 'hello',
+    'lives' => 3,
+    'correct' => 0,
+    'incorrect' => 0
   },
 
   :pTwo => {
-     "lives" => 3,
-     "correct" => 0,
-     "incorrect" => 0 
+     'name' => '',
+     'lives' => 3,
+     'correct' => 0,
+     'incorrect' => 0 
   }
 }
 
+@ap = true
 
 def question_generator
   @x = rand(20)
@@ -20,4 +23,27 @@ def question_generator
   puts "#{@z}"
 end
 
-question_generator
+def get_names
+  puts "Game time! Guess the answer to each question. You have three lives."
+  puts "The winner...they are the who doesn't lose all their lives."
+  puts "Player 1, enter your name:"
+  @players[:pOne]['name'] = gets.chomp
+  puts "Hi #{@players[:pOne]['name']}"
+  puts "Player 2, enter your name:"
+  @players[:pTwo]['name'] = gets.chomp
+  puts "Hi #{@players[:pTwo]['name']}..."
+  puts "Lets get going! Player 1 starts."
+end
+
+def active_player
+  player = @ap ? @players[:pOne]['name'] : @players[:pTwo]['name']
+  puts "#{player}"
+end
+
+def question_engine
+end
+
+get_names
+
+#active_player
+#question_generator
