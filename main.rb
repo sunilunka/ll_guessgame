dca1aff594c4b00ff805bafadb0ee25dfbc1ab75
Sunil Unka <sunil.unka@gmail.com>
2014-07-11 18:45:25 +0000

    Created player hashes

---
+++ main.rb
@@ -0,0 +1,16 @@
+players = {
+  :pOne => {
+    "lives" => 3,
+    "correct" => 0,
+    "incorrect" => 0
+  },
+
+  :pTwo => {
+     "lives" => 3,
+     "correct" => 0,
+     "incorrect" => 0 
+  }
+}
+
+
+puts "#{players[:pTwo]['correct']}"
