783b1689353febfaa9a9fd39af520715d1154cf1
Sunil Unka <sunil.unka@gmail.com>
2014-07-11 21:10:43 +0000

    Completed all mandatory (and some of the optional assignments.

--- main.rb
+++ main.rb
@@ -1,16 +1,14 @@
 @players = {
   :pOne => {
-    'name' => 'hello',
+    'name' => '',
     'lives' => 3,
     'correct' => 0,
-    'incorrect' => 0
   },
 
   :pTwo => {
      'name' => '',
      'lives' => 3,
      'correct' => 0,
-     'incorrect' => 0 
   }
 }
 
@@ -27,18 +25,6 @@ def question_generator
   @numbers << z
 end
 
-def get_names
-  puts "Game time! Guess the answer to each question. You have three lives."
-  puts "The winner...they are the who doesn't lose all their lives."
-  puts "Player 1, enter your name:"
-  @players[:pOne]['name'] = gets.chomp
-  puts "Hi #{@players[:pOne]['name']}"
-  puts "Player 2, enter your name:"
-  @players[:pTwo]['name'] = gets.chomp
-  puts "Hi #{@players[:pTwo]['name']}..."
-  puts "Lets get going! Player 1 starts."
-end
-
 def active_player
   player = @ap ? @players[:pOne] : @players[:pTwo]
   return player
@@ -48,12 +34,19 @@ def assess_answer(player, answer)
   correct = @numbers[2]
   if answer == correct
     player['correct'] += 1
+    puts "Nice one, that's correct!"
     puts "#{player['name']} score is #{player['correct']}"
+    puts 
   elsif answer != correct
     player['lives'] -= 1
     puts "That's incorrect"
-    puts "#{@players[:pOne]['name']} has #{@players[:pOne]['lives']}"
-    puts "#{@players[:pTwo]['name']} has #{@players[:pTwo]['lives']}"
+    puts "#{@players[:pOne]['name'].capitalize} has:"
+    puts "#{@players[:pOne]['lives']} lives remaining,"
+    puts "And a score of #{@players[:pOne]['correct']}"
+    puts 
+    puts "#{@players[:pTwo]['name']} has:" 
+    puts "#{@players[:pTwo]['lives']} lives remaining,"
+    puts "And a score of #{@players[:pTwo]['correct']}"
    end
 end
 
@@ -63,7 +56,6 @@ def get_answer(player)
   if (/[^[:digit:]]+/.match(answer).nil?)
     ans_num = answer.to_i
     assess_answer(player, ans_num)
-    puts "#{ans_num}"
   elsif !(/[^[:digit:]]+/.match(answer).nil?)
     puts "Sorry that is not a number. Try solving this one with numbers!"
     question_engine(player)
@@ -75,11 +67,41 @@ def question_engine(player)
   while (@players[:pOne]['lives'] > 0 && @players[:pTwo]['lives'] > 0)
     question_generator
     get_answer(active_player)
-    @one_lives = 0
+    @numbers.clear
+    @ap = !@ap
   end
+  try_again
 end
 
-#get_names
+def try_again
+  puts "GAME OVER."
+  puts "Would you like to try again?"
+  puts "Y or N"
+  option = gets.chomp
+  y_or_n = option.downcase
+  case y_or_n
+   when "y"
+    question_engine(active_player)
+  when "n"
+    puts "Sweet, see you next time!"
+    exit
+  else
+    puts "That's not an option."
+  end 
+end
 
-active_player
-question_engine(active_player)
+def get_names
+  puts "Game time! Guess the answer to each question. You have three lives."
+  puts "The winner...they are the who doesn't lose all their lives."
+  puts "Player 1, enter your name:"
+  @players[:pOne]['name'] = gets.chomp
+  puts "Hi #{@players[:pOne]['name']}"
+  puts "Player 2, enter your name:"
+  @players[:pTwo]['name'] = gets.chomp
+  puts "Hi #{@players[:pTwo]['name']}..."
+  puts "Lets get going! Player 1 starts."
+  question_engine(active_player)
+end
+
+
+get_names
